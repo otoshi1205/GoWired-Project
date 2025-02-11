c3616e93d09dc7418a06cf3dce34258907555fc1
Tomasz Gos <gomektos@gmail.com>
2025-02-11 16:36:59 +0100

    Deleted unsupported ArduinoSTL library

--- Software/Modules/Arduino/main/custom_types.h
+++ Software/Modules/Arduino/main/custom_types.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <ArduinoSTL.h>
-#include <array>
 #include <core/CommonIO.h>
 #include "Configuration.h"
 
-using CommonIOPins = std::array<CommonIO, NUMBER_OF_RELAYS + NUMBER_OF_INPUTS>;
+using CommonIOPins = CommonIO[NUMBER_OF_RELAYS + NUMBER_OF_INPUTS];
